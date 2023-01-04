7a1d1f3b252d4125afec98696450dc66b7ba182b
gashug <geoffrey.gachuiri98@gmail.com>
2023-01-04 12:11:19 -0800

    a few changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
-if __name__ == '__main__':
-    for n in range(100):
-        if n != 99:
-            print('{:02d}'.format(n), end=', ')
-        else:
-            print('{:02d}'.format(n))
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
