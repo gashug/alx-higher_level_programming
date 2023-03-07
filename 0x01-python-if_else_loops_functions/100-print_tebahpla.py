1e7a09a91c08bb4ee48edd108465cb6fe303bec5
gashug <geoffrey.gachuiri98@gmail.com>
2023-03-07 10:50:20 -0800

    Fixed 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(122, 97, -2):
-    for j in range(89, 64, -2):
-        print("{}{}".format(chr(i), chr(j), end=""))
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i -= 32
+    print("{}".format(chr(i)), end="")
 
