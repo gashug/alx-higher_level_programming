e3cde3bd55e814e8dbe4973c387ed7d96926aef5
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-05 19:56:04 -0700

    Fixed several bugs

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -15,9 +15,9 @@ class Rectangle:
             width(int): The width of the new square
             height(int): The height of the new square
         """
-        type(self).number_of_instances += 1
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -76,7 +76,7 @@ class Rectangle:
 
     def __del__(self):
         """Deletes an instance of a rectangle"""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @staticmethod
