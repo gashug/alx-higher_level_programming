1c9705dfee5d9e11de8b82cc093d4224d5194534
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-13 05:33:57 -0700

    added tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
 
-for arg in argv[1:]:
-    my_list.append(arg)
+    for arg in argv[1:]:
+        my_list.append(arg)
 
-save_to_json_file(my_list, "add_item.json")
+    save_to_json_file(my_list, "add_item.json")
