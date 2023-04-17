06ffd857e0bc3460b4b9be354528c1ceb0007589
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-17 08:09:15 -0700

    correction for 3-is_kind_of...

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """object instance checking function"""
 
+
 def is_kind_of_class(obj, a_class):
     """returns True if object is instance of specified class
         otherwise False
