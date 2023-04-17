f699a919c7d48d8467218b5dd988dd481609b6bf
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-17 08:54:42 -0700

    correction for 4_inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """object instance checking function"""
 
+
 def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a class 
         inherited from a specified class"""
