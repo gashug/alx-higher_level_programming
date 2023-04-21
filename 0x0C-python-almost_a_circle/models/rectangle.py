08092f02b396cfe8db7c8623030474d983bad1a7
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-21 01:34:29 -0700

    update to rectangle class for display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints in std out rectangle instance with #"""
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(' ' * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Returns string rep of rectangle object"""
