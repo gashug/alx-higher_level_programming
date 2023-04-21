c4fd331f94607edcef994ae093c422db61e2806f
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-21 01:13:33 -0700

    update to rectangle class for display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns area of the Rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints in std out rectangle instance with #"""
+        for i in range(self.__height):
+                print("#" * self.__width)
