9400e9fc2638322a66677e8ce4315e1fdc4f3ce8
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-21 14:28:04 -0700

    Added some advanced tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """defines the class Base"""
 import json
-
+import csv
+import turtle
 
 class Base():
     """"empty class Base"""
@@ -67,3 +68,57 @@ class Base():
                 return list_instances
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", newline='') as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, mode="r", newline='') as file:
+                reader = csv.reader(file)
+                objs = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        d = {'id': int(row[0]), 'width': int(row[1]), 'height': int(row[2]), 'x': int(row[3]), 'y': int(row[4])}
+                    elif cls.__name__ == "Square":
+                        d = {'id': int(row[0]), 'size': int(row[1]), 'x': int(row[2]), 'y': int(row[3])}
+                    objs.append(cls.create(**d))
+                return objs
+        except FileNotFoundError:
+            return []
+
+        @staticmethod
+    def draw(list_rectangles, list_squares):
+        turtle.bgcolor("white")
+        turtle.title("Draw Shapes")
+        turtle.penup()
+        turtle.goto(-300, 0)
+
+        for shape in list_rectangles + list_squares:
+            turtle.setheading(0)
+            turtle.goto(shape.x, shape.y)
+            turtle.pendown()
+            turtle.color("red")
+            turtle.begin_fill()
+            for _ in range(2):
+                turtle.forward(shape.width)
+                turtle.left(90)
+                turtle.forward(shape.height)
+                turtle.left(90)
+            turtle.end_fill()
+            turtle.penup()
+
+        turtle.hideturtle()
+        turtle.done()
