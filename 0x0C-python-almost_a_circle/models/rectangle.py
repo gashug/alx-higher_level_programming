e28d29ad09995634e212352cc74f9b3bed4bcddf
gashug <geoffrey.gachuiri98@gmail.com>
2023-04-21 01:20:38 -0700

    update to rectangle class for string object

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,7 @@ class Rectangle(Base):
         """Prints in std out rectangle instance with #"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Returns string rep of rectangle object"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
