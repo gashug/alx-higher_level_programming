1f313a81a5859c89a015dec8e8c37c83d08a672f
gashug <geoffrey.gachuiri98@gmail.com>
2023-05-11 00:24:12 -0700

    Fixed 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -76,5 +76,5 @@ class Rectangle:
 
     def __del__(self):
         """Deletes an instance of a rectangle"""
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
