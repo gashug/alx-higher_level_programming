335d3118d45fb497a792aa5f7f0854ad7895e7e4
gashug <geoffrey.gachuiri98@gmail.com>
2023-05-11 02:02:28 -0700

    Corrected 1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ class MyList(list):
     """
     Custom list class that inherits from list.
     """
-    def __init__(self):
-        """Initialize the object"""
-        super().__init__()
-
     def print_sorted(self):
         """Prints list in sorted order"""
         print(sorted(self))
