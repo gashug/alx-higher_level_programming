8c0a68d0ca14004b56784e605d9aab71f20c36d4
gashug <geoffrey.gachuiri98@gmail.com>
2023-05-16 13:17:34 -0700

    Added some mandatory tasks

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- deletes a created database in the MySQL server
+-- if database doesn't exist script shouldn't fail
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all tables of a database in MySQL server.
+
+SHOW TABLES;
