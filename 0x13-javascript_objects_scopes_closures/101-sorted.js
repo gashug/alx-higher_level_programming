0436b954189eec0840d0b5e24b8cba3879c50599
gashug <geoffrey.gachuiri98@gmail.com>
2023-06-13 14:24:00 -0700

    Added some heavy tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+
+const occurrencesByUserId = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (!occurrencesByUserId.hasOwnProperty(occurrences)) {
+    occurrencesByUserId[occurrences] = [];
+  }
+
+  occurrencesByUserId[occurrences].push(userId);
+}
+
+console.log(occurrencesByUserId);
