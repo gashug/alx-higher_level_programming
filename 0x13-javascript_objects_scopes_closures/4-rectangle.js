ffb28822804f1e8a25bbedf2b24f0bb33c0deb36
gashug <geoffrey.gachuiri98@gmail.com>
2023-06-13 13:54:58 -0700

    Mandatory tasks added

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return 'Rectangle {}';
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      for (let j = 1; j <= this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+module.exports = Rectangle;
