7c09c33accb0529ab43b575d4b6f10eaf11bd8c5
gashug <geoffrey.gachuiri98@gmail.com>
2023-06-13 14:27:14 -0700

    Added some heavy tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,13 +4,15 @@ const { dict } = require('./101-data');
 const occurrencesByUserId = {};
 
 for (const userId in dict) {
-  const occurrences = dict[userId];
+  if (Object.prototype.hasOwnProperty.call(dict, userId)) {
+    const occurrences = dict[userId];
 
-  if (!occurrencesByUserId.hasOwnProperty(occurrences)) {
-    occurrencesByUserId[occurrences] = [];
+    if (!Object.prototype.hasOwnProperty.call(occurrencesByUserId, occurrences)) {
+      occurrencesByUserId[occurrences] = [];
+    }
+
+    occurrencesByUserId[occurrences].push(userId);
   }
-
-  occurrencesByUserId[occurrences].push(userId);
 }
 
 console.log(occurrencesByUserId);
