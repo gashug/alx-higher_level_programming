f258c563d7da7a165b99c814f774c0ef7a09fa42
gashug <geoffrey.gachuiri98@gmail.com>
2023-10-24 11:20:04 -0700

    corrected a few tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,14 +2,11 @@
 
 const request = require('request');
 
-// Define the base URL for the Star Wars API (SWAPI).
-const baseURL = 'https://swapi.dev/api/';
+const baseURL = 'https://swapi-api.alx-tools.com/api/';
 
-// Function to fetch and print character names for a specific movie.
 function printStarWarsCharacters(movieId) {
   const filmURL = `${baseURL}films/${movieId}/`;
 
-  // Make an HTTP GET request to retrieve information about the specified movie.
   request(filmURL, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       console.error('Error fetching movie data');
@@ -23,13 +20,11 @@ function printStarWarsCharacters(movieId) {
       return;
     }
 
-    // Fetch character data and print their names.
     const characterURLs = filmData.characters;
     fetchAndPrintCharacters(characterURLs, 0);
   });
 }
 
-// Recursive function to fetch and print character names.
 function fetchAndPrintCharacters(characterURLs, index) {
   if (index >= characterURLs.length) {
     return;
@@ -40,7 +35,6 @@ function fetchAndPrintCharacters(characterURLs, index) {
       const characterData = JSON.parse(body);
       console.log(characterData.name);
 
-      // Fetch and print the next character.
       fetchAndPrintCharacters(characterURLs, index + 1);
     } else {
       console.error(`Error fetching character data for character ${index + 1}`);
@@ -48,7 +42,6 @@ function fetchAndPrintCharacters(characterURLs, index) {
   });
 }
 
-// Check if the Movie ID is provided as a command-line argument.
 if (process.argv.length !== 3) {
   console.error('Usage: node 101-starwars_characters.js <Movie ID>');
   process.exit(1);
@@ -56,5 +49,4 @@ if (process.argv.length !== 3) {
 
 const movieId = process.argv[2];
 
-// Call the main function to start the process.
 printStarWarsCharacters(movieId);
